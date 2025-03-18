c489b7257187395d6a24226da41162859d0d767b
leency <lipatov.kiril@gmail.com>
2025-03-18 22:14:43 +0200

    macros.inc: replace unused RECT struct with 'area', use it in pipet

--- programs/other/pipet/pipet.asm
+++ programs/other/pipet/pipet.asm
@@ -129,11 +129,11 @@ draw_window:
         mcall                    , SSF_GET_SKIN_HEIGHT,
 
         mov     ecx, eax
-        add     ecx, WIN_Y * 65536 + WIN_H
+        add     ecx, WIN.Y * 65536 + WIN.H
 
         mov     edx, [win_cols.work]
         add     edx, 0x34000000
-        mcall   SF_CREATE_WINDOW, <WIN_X, WIN_W>, , , , header
+        mcall   SF_CREATE_WINDOW, <WIN.X, WIN.W>, , , , header
 
         call    draw_base
         call    draw_update
@@ -147,30 +147,30 @@ draw_window:
 ; draw basic elements of window
 draw_base:
 
-        mcall   SF_DRAW_RECT, <BUT_PIP_X, BUT_PIP_H>, <BUT_PIP_Y, BUT_PIP_H>, [win_cols.work_graph]
-        mcall               , <BUT_COL_X, BUT_COL_W>,                       ,
-        mcall               , <BUT_HEX_X, BUT_HEX_W>, <BUT_HEX_Y, BUT_HEX_H>,
-        mcall               ,                       , <BUT_RGB_Y, BUT_HEX_H>,
-        mcall               , <BUT_REC_X, BUT_REC_W>, <BUT_REC_Y, BUT_REC_H>,
+        mcall   SF_DRAW_RECT, <BUT_PIP.X, BUT_PIP.H>, <BUT_PIP.Y, BUT_PIP.H>, [win_cols.work_graph]
+        mcall               , <BUT_COL.X, BUT_COL.W>,                       ,
+        mcall               , <BUT_HEX.X, BUT_HEX.W>, <BUT_HEX.Y, BUT_HEX.H>,
+        mcall               ,                       , <BUT_RGB.Y, BUT_HEX.H>,
+        mcall               , <BUT_REC.X, BUT_REC.W>, <BUT_REC.Y, BUT_REC.H>,
 
-        mcall               , <BUT_PIP_X, BUT_PIP_H - 1>, <BUT_PIP_Y, BUT_PIP_H - 1>, [win_cols.work_dark]
-        mcall               , <BUT_COL_X, BUT_COL_W - 1>,                           ,
-        mcall               , <BUT_HEX_X, BUT_HEX_W - 1>, <BUT_HEX_Y, BUT_HEX_H - 1>,
-        mcall               ,                           , <BUT_RGB_Y, BUT_HEX_H - 1>,
-        mcall               , <BUT_REC_X, BUT_REC_W - 1>, <BUT_REC_Y, BUT_REC_H - 1>,
+        mcall               , <BUT_PIP.X, BUT_PIP.H - 1>, <BUT_PIP.Y, BUT_PIP.H - 1>, [win_cols.work_dark]
+        mcall               , <BUT_COL.X, BUT_COL.W - 1>,                           ,
+        mcall               , <BUT_HEX.X, BUT_HEX.W - 1>, <BUT_HEX.Y, BUT_HEX.H - 1>,
+        mcall               ,                           , <BUT_RGB.Y, BUT_HEX.H - 1>,
+        mcall               , <BUT_REC.X, BUT_REC.W - 1>, <BUT_REC.Y, BUT_REC.H - 1>,
 
-        mcall               , <BUT_PIP_X + 1, BUT_PIP_H - 2>, <BUT_PIP_Y + 1, BUT_PIP_H - 2>, [col_white]
-        mcall               , <BUT_COL_X + 1, BUT_COL_W - 2>,                               ,
-        mcall               , <BUT_HEX_X + 1, BUT_HEX_W - 2>, <BUT_HEX_Y + 1, BUT_HEX_H - 2>,
-        mcall               ,                               , <BUT_RGB_Y + 1, BUT_HEX_H - 2>,
-        mcall               , <BUT_REC_X + 1, BUT_REC_W - 2>, <BUT_REC_Y + 1, BUT_REC_H - 2>,
+        mcall               , <BUT_PIP.X + 1, BUT_PIP.H - 2>, <BUT_PIP.Y + 1, BUT_PIP.H - 2>, [col_white]
+        mcall               , <BUT_COL.X + 1, BUT_COL.W - 2>,                               ,
+        mcall               , <BUT_HEX.X + 1, BUT_HEX.W - 2>, <BUT_HEX.Y + 1, BUT_HEX.H - 2>,
+        mcall               ,                               , <BUT_RGB.Y + 1, BUT_HEX.H - 2>,
+        mcall               , <BUT_REC.X + 1, BUT_REC.W - 2>, <BUT_REC.Y + 1, BUT_REC.H - 2>,
 
 
         ; buttons 11, 12, 13 and 14
-        mcall   SF_DEFINE_BUTTON, <BUT_PIP_X + 1, BUT_PIP_W - 3>, <BUT_PIP_Y + 1, BUT_PIP_H - 3>, 0x4000000B
-        mcall                   , <BUT_HEX_X + 1, BUT_HEX_W - 3>, <BUT_HEX_Y + 1, BUT_HEX_H - 3>, 0x4000000C
-        mcall                   ,                               , <BUT_RGB_Y + 1, BUT_RGB_H - 3>, 0x4000000D
-        mcall                   , <BUT_REC_X + 2, BUT_REC_W - 4>, <BUT_REC_Y + 2, BUT_REC_H - 4>, 0x6000000E
+        mcall   SF_DEFINE_BUTTON, <BUT_PIP.X + 1, BUT_PIP.W - 3>, <BUT_PIP.Y + 1, BUT_PIP.H - 3>, 0x4000000B
+        mcall                   , <BUT_HEX.X + 1, BUT_HEX.W - 3>, <BUT_HEX.Y + 1, BUT_HEX.H - 3>, 0x4000000C
+        mcall                   ,                               , <BUT_RGB.Y + 1, BUT_RGB.H - 3>, 0x4000000D
+        mcall                   , <BUT_REC.X + 2, BUT_REC.W - 4>, <BUT_REC.Y + 2, BUT_REC.H - 4>, 0x6000000E
 
         ; 18*18 pixels icon
         mcall   SF_PUT_IMAGE_EXT, [pip_icon], 0x00120012, 0x000B000F, 32
@@ -182,12 +182,12 @@ draw_base:
 draw_update:
 
         ; current color rect
-        mcall   SF_DRAW_RECT, <BUT_COL_X + 2, BUT_COL_W - 4>, <BUT_COL_Y + 2, BUT_COL_H - 4>, [sel_color]
+        mcall   SF_DRAW_RECT, <BUT_COL.X + 2, BUT_COL.W - 4>, <BUT_COL.Y + 2, BUT_COL.H - 4>, [sel_color]
 
         ; color codes
-        mcall               , <BUT_RGB_X + 1, BUT_HEX_W - 2>, <BUT_RGB_Y + 1, BUT_HEX_H - 2>, [col_white]
+        mcall               , <BUT_RGB.X + 1, BUT_HEX.W - 2>, <BUT_RGB.Y + 1, BUT_HEX.H - 2>, [col_white]
 
-        mcall   SF_DRAW_NUMBER, 0x00060100, [sel_color], <BUT_HEX_X + 26, BUT_HEX_Y + 5>, 0x50000000, [col_white]
+        mcall   SF_DRAW_NUMBER, 0x00060100, [sel_color], <BUT_HEX.X + 26, BUT_HEX.Y + 5>, 0x50000000, [col_white]
 
         mov     ebx, 0x00030000
         xor     ecx, ecx
@@ -257,12 +257,12 @@ make_pick_active:
         mov     [cell_act_x], 0x03
         mov     [cell_act_y], 0x03
 
-        mcall   SF_DRAW_RECT, <BUT_REC_X + 1, BUT_REC_W - 2>, <BUT_REC_Y + 1, BUT_REC_H - 2>, [col_white]
-        mcall   SF_DRAW_TEXT, <BUT_REC_X + 28, BUT_REC_Y + 37>, 0x10000000, mes_pick, 4
+        mcall   SF_DRAW_RECT, <BUT_REC.X + 1, BUT_REC.W - 2>, <BUT_REC.Y + 1, BUT_REC.H - 2>, [col_white]
+        mcall   SF_DRAW_TEXT, <BUT_REC.X + 28, BUT_REC.Y + 37>, 0x10000000, mes_pick, 4
 
         mcall   SF_SLEEP, 50
 
-        mcall   SF_DRAW_RECT, <BUT_PIP_X + 1, BUT_PIP_W - 2>, <BUT_PIP_Y + 1, BUT_PIP_H - 2>, [win_cols.work_light]
+        mcall   SF_DRAW_RECT, <BUT_PIP.X + 1, BUT_PIP.W - 2>, <BUT_PIP.Y + 1, BUT_PIP.H - 2>, [win_cols.work_light]
         mcall   SF_PUT_IMAGE_EXT, [pip_icon], 0x00120012, 0x000B000F, 32
 
         call    draw_update
@@ -340,7 +340,7 @@ clamp_pixels:
 ; copy color HEX code
 copy_col_hex:
 
-        mcall   SF_DRAW_RECT, <BUT_HEX_X + 2, BUT_HEX_W - 4>, <BUT_HEX_Y + 2, BUT_HEX_H - 4>, [col_white]
+        mcall   SF_DRAW_RECT, <BUT_HEX.X + 2, BUT_HEX.W - 4>, <BUT_HEX.Y + 2, BUT_HEX.H - 4>, [col_white]
 
         mov     ebx, [sel_color]
         mov     ecx, 6
@@ -360,10 +360,10 @@ copy_col_hex:
                         loop    ch_loop
 
         mcall   SF_CLIPBOARD, 2, color_hex.end - color_hex, color_hex
-        mcall   SF_DRAW_TEXT, <BUT_HEX_X + 1, BUT_HEX_Y + 5>, 0x10000000, mes_copy, 12
+        mcall   SF_DRAW_TEXT, <BUT_HEX.X + 1, BUT_HEX.Y + 5>, 0x10000000, mes_copy, 12
         mcall   SF_SLEEP, 50
 
-        mcall   SF_DRAW_RECT, <BUT_HEX_X + 1, BUT_HEX_W - 2>, <BUT_HEX_Y + 1, BUT_HEX_H - 2>, [col_white]
+        mcall   SF_DRAW_RECT, <BUT_HEX.X + 1, BUT_HEX.W - 2>, <BUT_HEX.Y + 1, BUT_HEX.H - 2>, [col_white]
 
         call    draw_update
         jmp     still
@@ -372,7 +372,7 @@ copy_col_hex:
 ; copy color RGB code
 copy_col_rgb:
 
-        mcall   SF_DRAW_RECT, <BUT_HEX_X + 2, BUT_HEX_W - 4>, <BUT_RGB_Y + 2, BUT_HEX_H - 4>, [col_white]
+        mcall   SF_DRAW_RECT, <BUT_HEX.X + 2, BUT_HEX.W - 4>, <BUT_RGB.Y + 2, BUT_HEX.H - 4>, [col_white]
 
         mov     bl, 10
         mov     edx, [sel_color]
@@ -400,10 +400,10 @@ copy_col_rgb:
 
 
         mcall   SF_CLIPBOARD, 2, color_rgb.end - color_rgb, color_rgb
-        mcall   SF_DRAW_TEXT, <BUT_HEX_X + 1, BUT_RGB_Y + 5>, 0x10000000, mes_copy, 12
+        mcall   SF_DRAW_TEXT, <BUT_HEX.X + 1, BUT_RGB.Y + 5>, 0x10000000, mes_copy, 12
         mcall   SF_SLEEP, 50
 
-        mcall   SF_DRAW_RECT, <BUT_HEX_X + 1, BUT_HEX_W - 2>, <BUT_RGB_Y + 1, BUT_HEX_H - 2>, [col_white]
+        mcall   SF_DRAW_RECT, <BUT_HEX.X + 1, BUT_HEX.W - 2>, <BUT_RGB.Y + 1, BUT_HEX.H - 2>, [col_white]
 
         call    draw_update
         jmp     still
@@ -444,35 +444,13 @@ pick_col_cell:
 
 ;---------------------------------------------------------------------
 
-WIN_X           = 100
-WIN_W           = 221
-WIN_Y           = 100
-WIN_H           = 112
+WIN     area 100,100,221,112
 
-BUT_PIP_X       = 8
-BUT_PIP_W       = 24
-BUT_PIP_Y       = 12
-BUT_PIP_H       = 24
-
-BUT_COL_X       = 40
-BUT_COL_W       = 68
-BUT_COL_Y       = 12
-BUT_COL_H       = 24
-
-BUT_HEX_X       = 8
-BUT_HEX_W       = 100
-BUT_HEX_Y       = 44
-BUT_HEX_H       = 24
-
-BUT_RGB_X       = 8
-BUT_RGB_W       = 100
-BUT_RGB_Y       = 76
-BUT_RGB_H       = 24
-
-BUT_REC_X       = 116
-BUT_REC_W       = 88
-BUT_REC_Y       = 12
-BUT_REC_H       = 88
+BUT_PIP area   8, 12, 24, 24
+BUT_COL area  40, 12, 68, 24
+BUT_HEX area   8, 44,100, 24
+BUT_RGB area   8, 76,100, 24
+BUT_REC area 116, 12, 88, 88
 
 ;---------------------------------------------------------------------
 
