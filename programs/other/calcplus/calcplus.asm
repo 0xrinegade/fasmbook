e02bea3d1e0a3a6ff02c675636f11bad77dca63b
Burer <burer@kolibrios.org>
2025-04-17 10:45:36 +0300

    [Apps/Calcplus] Fix list buttons not working

--- programs/other/calcplus/calcplus.asm
+++ programs/other/calcplus/calcplus.asm
@@ -561,7 +561,7 @@ proc draw_list
         mov     eax, SF_DEFINE_BUTTON
         mov     ebx, LIST_X shl 16 + (LIST_ITEM_W - 1)
         mov     ecx, LIST_Y shl 16 + LIST_ITEM_H
-        mov     edx, 60 + BT_HIDE
+        mov     edx, 0x60 + BT_HIDE
         mov     edi, LIST_ITEM_COUNT
 
         @@:
